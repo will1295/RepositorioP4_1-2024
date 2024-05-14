36e839f6443c73d5403e872737161b9550667675
Will1295 <willian120795@gmail.com>
2024-05-13 19:03:56 -0600

    agregando vue

---
+++ A1/practica1_lun13/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <MiComponte/>
+  <!--<HelloWorld msg="Estoy desde vue" msg2="Y esto es una propiedad"/>-->
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import MiComponte from './components/MiComponente.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MiComponte
+    //HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ A1/practica1_lun13/src/components/MiComponente.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>
+            {{parrafo}}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'MiComponente',
+        data(){
+            return {
+            parrafo:"lorem ipsum it dolor"
+        }
+        }
+    }
+</script>
+
+<style scoped>
+    div{
+        color: red;
+    }
+</style>
