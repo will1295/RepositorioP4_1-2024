a5db98f75dabfc4a2f79f9ea90b556193c751ec4
Will1295 <willian120795@gmail.com>
2024-05-16 18:28:08 -0600

    Actualizando repositorio

---
+++ B2/s17_b2/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <BindComponent></BindComponent>
+</template>
+
+<script>
+import BindComponent from './components/BindComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BindComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ B2/s17_b2/src/components/BindComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <p :id="idparrafo">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+         Nunc leo nunc, cursus eu scelerisque et, congue a
+          justo. Nulla et tristique lorem. Fusce sit 
+          amet nibh vitae risus imperdiet venenatis non sed 
+          nisl.
+    </p>
+
+    <div>
+        <ul>
+            <li v-for="producto,index in productos" :key="index">{{producto }}</li>
+        </ul>
+
+    </div>
+
+    <button @click="alerta">{{ txtbutton }}</button>
+
+    <input type="text" v-model="inputText">
+    <p>{{ inputText }}</p>
+    <p v-html="textoHtml"></p>
+
+</template>
+
+<script>
+
+export default{
+    name:'BindComponent',
+    data(){
+        return{
+            inputText:'',
+            idparrafo:'parrafo',
+            txtbutton:'Haz Click Aqui',
+            productos:[
+                'Papas',
+                'Cebollas',
+                'Tomates'
+            ],
+            textoHtml:'<span style="color:red">Parrafo en rojo</span>'
+            
+        }
+    },
+    methods:{
+        alerta(){
+            alert('Hola Vue!')
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+    #parrafo{
+        color: red;
+    }
+
+</style>
